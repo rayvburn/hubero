311a2f4bc1e6ffc3b03fd155d364b87c01c1d14f
rayvburn <karwowsj@ee.pw.edu.pl>
2018-12-16 13:30:12 +0100

    Add normal distribution of SFM parameters

--- actor_plugin_social/include/SocialForceModel.h
+++ actor_plugin_social/include/SocialForceModel.h
@@ -8,21 +8,24 @@
 #ifndef INCLUDE_SOCIALFORCEMODEL_H_
 #define INCLUDE_SOCIALFORCEMODEL_H_
 
-#include <ignition/math.hh>
+#include <SocialForceModelUtils.h>
+#include <ignition/math.hh> // ?? needed still
 
-/* Calculations are based on:
- * 		- D. Helbing's - Social Force Model for Pedestrian Dynamics ‎(1998)
- * 		- Moussaid et al., 2009
+// ----------------------------------------------------------------------------------------------- //
+/* References:
+ * 		- D. Helbing et al. 	- Social Force Model for Pedestrian Dynamics ‎(1998)
+ * 		- Moussaid et al. 		- Experimental study of the behavioural mechanisms underlying
+ * 		  						  self-organization in human crowds (2009)
+ * 		- S. Seer et al. 		- Validating Social Force based Models with Comprehensive
+ * 		  						  Real World Motion Data (2014)
+ *
+ * https://www.sciencedirect.com/science/article/pii/S2352146514001161
+ * In the above paper there are results of the research presented. They point that
+ * Model C (Rudloff et al. (2011) of SFM fits best the real world data.
  */
+// ----------------------------------------------------------------------------------------------- //
 
 namespace SocialForceModel {
-
-enum ObjectType {
-	STATIC_REPULSIVE = 0,
-	DYNAMIC_REPULSIVE,
-	STATIC_ATTRACTIVE,
-	DYNAMIC_ATTRACTIVE
-};
 
 class SocialForceModel {
 
@@ -34,13 +37,36 @@ public:
 
 private:
 
-	double GetInternalAcceleration(ObjectType _type);
-	double GetRepulsion(ObjectType _type);
-	double GetAttraction(ObjectType _type);
-	ignition::math::Vector3d CalculateNumGradient(ignition::math::Vector3d &_vector);
+	void 	SetParameterValues(void);
+	double 	GetInternalAcceleration(SFMObjectType _type);
+	double 	GetRepulsion(SFMObjectType _type);
+	double 	GetAttraction(SFMObjectType _type);
 
-	float RELAXATION_TIME;
-	float DESIRED_SPEED;
+	float relaxation_time;
+	float speed_desired;
+	float speed_max;
+
+	// Rudloff et al. (2011) model's parameters based on  S. Seer et al. (2014)
+	float An;
+	float Bn;
+	float Cn;
+	float Ap;
+	float Bp;
+	float Cp;
+	float Aw;
+	float Bw;
+
+	/* setting parameters static will create a population of actors moving in the same way */
+	/*
+	static float An;
+	static float Bn;
+	static float Cn;
+	static float Ap;
+	static float Bp;
+	static float Cp;
+	static float Aw;
+	static float Bw;
+	*/
 
 };
 
--- actor_plugin_social/src/SocialForceModel.cpp
+++ actor_plugin_social/src/SocialForceModel.cpp
@@ -9,7 +9,13 @@
 
 namespace SocialForceModel {
 
-SocialForceModel::SocialForceModel() {
+// ------------------------------------------------------------------- //
+
+SocialForceModel::SocialForceModel():
+
+	speed_max(1.5) {
+
+	SetParameterValues();
 
 	/* Algorithm PARAMETERS are:
 	 * - relaxation time must be given here
@@ -18,7 +24,10 @@ SocialForceModel::SocialForceModel() {
 	 * - direction weight
 	 * - max speed
 	 */
+
 }
+
+// ------------------------------------------------------------------- //
 
 double SocialForceModel::GetSocialForce() {
 
@@ -48,6 +57,40 @@ double SocialForceModel::GetSocialForce() {
 
 }
 
+// ------------------------------------------------------------------- //
+
+void SocialForceModel::SetParameterValues() {
+
+	/* Invoking this function to each actor will create a population in which there are
+	 * everyone moving in slightly other way */
+
+	std::default_random_engine rand_gen;	// random number generator
+
+	// desired speed (based on (Moussaid et al. (2009))
+	std::normal_distribution<float> dist_spd_desired(1.29F, 0.19F);
+	speed_desired = dist_spd_desired(rand_gen);
+
+	// relaxation time (based on (Moussaid et al. (2009))
+	std::normal_distribution<float> dist_tau(0.54F, 0.05F);
+	relaxation_time = dist_tau(rand_gen);
+
+	// ----------------------------- Model C ------------------------------------------------------ //
+	// Rudloff et al. (2011) model's parameters based on  S. Seer et al. (2014)
+	// Generate random value of mean a and standard deviation b
+	std::normal_distribution<float> dist_an(0.2615F, 0.0551F);		An = dist_an(rand_gen);
+	std::normal_distribution<float> dist_bn(0.4026F, 0.1238F);		Bn = dist_bn(rand_gen);
+	std::normal_distribution<float> dist_cn(2.1614F, 0.3728F);		Cn = dist_cn(rand_gen);
+	std::normal_distribution<float> dist_ap(1.5375F, 0.3084F);		Ap = dist_ap(rand_gen);
+	std::normal_distribution<float> dist_bp(0.4938F, 0.1041F);		Bp = dist_bp(rand_gen);
+	std::normal_distribution<float> dist_cp(0.5710F, 0.1409F);		Cp = dist_cp(rand_gen);
+	std::normal_distribution<float> dist_aw(0.3280F, 0.1481F);		Aw = dist_aw(rand_gen);
+	std::normal_distribution<float> dist_bw(0.1871F, 0.0563F);		Bw = dist_bw(rand_gen);
+
+}
+
+// ------------------------------------------------------------------- //
+
+/* DEPRECATED
 ignition::math::Vector3d SocialForceModel::CalculateNumGradient(ignition::math::Vector3d &_vector) {
 
 	// Vector3D is just for convenience (to avoid conversion)
@@ -56,6 +99,9 @@ ignition::math::Vector3d SocialForceModel::CalculateNumGradient(ignition::math::
 	return num_gradient;
 
 }
+*/
+
+// ------------------------------------------------------------------- //
 
 SocialForceModel::~SocialForceModel() { }
 
