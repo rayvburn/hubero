44426ac6b5d800eeac035ba40f55175051d7132d
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2020-01-05 03:32:37 +0100

    WIP - SFM - additional weakening of the internal force component as some obstacle is very near, needs further evaluation

--- actor_plugin_social/src/sfm/core/SocialForceModel.cpp
+++ actor_plugin_social/src/sfm/core/SocialForceModel.cpp
@@ -1880,6 +1880,15 @@ void SocialForceModel::applyNonlinearOperations(const double &dist_closest_stati
 	// truncate the force value to max to prevent strange speedup of an actor
 	double force_combined_magnitude_init = force_combined_.Length();
 
+	// TODO: additional factor which weakens internal component when there is
+	// an obstacle close to the actor
+	// FIXME: experimental
+	double internal_weakening_factor = std::exp(0.75 * dist_closest) - 1.0;
+	if ( internal_weakening_factor > 1.0 ) {
+		internal_weakening_factor = 1.0;
+	}
+	force_internal_ *= internal_weakening_factor;
+
 	// evaluate force magnitude
 	if ( force_combined_magnitude_init >= force_max_ ) {
 
