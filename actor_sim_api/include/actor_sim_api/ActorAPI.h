d0a293e63f2ea905060dedc953152634736cae47
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2020-02-27 01:45:18 +0100

    WIP - API extension (remaining services)

--- actor_sim_api/include/actor_sim_api/ActorAPI.h
+++ actor_sim_api/include/actor_sim_api/ActorAPI.h
@@ -11,6 +11,8 @@
 #include <ros/ros.h>
 #include <actor_sim_srv/GetPose.h>
 #include <actor_sim_srv/GetVelocity.h>
+#include <actor_sim_srv/SetStance.h>
+#include <std_srvs/Trigger.h>
 
 class ActorAPI {
 
@@ -18,6 +20,11 @@ public:
 
 	/// \brief Parametrized constructor
 	ActorAPI(const std::string &actor_ns_name, ros::NodeHandle *nh_ptr);
+
+	bool moveAroundStart();
+	bool moveAroundStop();
+
+	bool setStance(const uint8_t &stance_type);
 
 	/// \brief Get velocity vector (linear x, linear y and angular `yaw`)
 	std::array<double, 3> getVelocity(); // cannot be const
@@ -38,6 +45,15 @@ protected:
 
 	/// \brief `get_pose` service client (`sc`)
 	ros::ServiceClient sc_get_pose_;
+
+	/// \brief `move_around_start` service client (`sc`)
+	ros::ServiceClient sc_move_around_start_;
+
+	/// \brief `move_around_stop` service client (`sc`)
+	ros::ServiceClient sc_move_around_stop_;
+
+	/// \brief `set_stance` service client (`sc`)
+	ros::ServiceClient sc_set_stance_;
 
 };
 
--- actor_sim_api/src/actor_sim_api/ActorAPI.cpp
+++ actor_sim_api/src/actor_sim_api/ActorAPI.cpp
@@ -13,6 +13,38 @@ ActorAPI::ActorAPI(const std::string &actor_ns_name, ros::NodeHandle *nh_ptr) {
 	sc_get_velocity_ = nh_ptr->serviceClient<actor_sim_srv::GetVelocity>(actor_ns_name + "/get_velocity");
 	sc_get_pose_ = nh_ptr->serviceClient<actor_sim_srv::GetVelocity>(actor_ns_name + "/get_pose");
 
+	sc_move_around_start_ = nh_ptr->serviceClient<std_srvs::Trigger>(actor_ns_name + "/move_around");
+	sc_move_around_stop_ = nh_ptr->serviceClient<std_srvs::Trigger>(actor_ns_name + "/move_around_stop");
+	sc_set_stance_ = nh_ptr->serviceClient<actor_sim_srv::SetStance>(actor_ns_name + "/set_stance");
+
+}
+
+// ---------------------------------------------------------------------------------
+
+bool ActorAPI::moveAroundStart() {
+
+	std_srvs::Trigger srv;
+	return (sc_move_around_start_.call(srv));
+
+}
+
+// ---------------------------------------------------------------------------------
+
+bool ActorAPI::moveAroundStop() {
+
+	std_srvs::Trigger srv;
+	return (sc_move_around_stop_.call(srv));
+
+}
+
+// ---------------------------------------------------------------------------------
+
+bool ActorAPI::setStance(const uint8_t &stance_type) {
+
+	actor_sim_srv::SetStance srv;
+	srv.request.stance_enum = stance_type;
+	return (sc_set_stance_.call(srv));
+
 }
 
 // ---------------------------------------------------------------------------------
