9cc63ad018542f025f1d76215235d15065330482
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-01-09 05:01:45 +0100

    complete remake of ActorPlugin (hubero_gazebo), interfaces for hubero Actor created (hubero_gazebo + hubero_core)

--- hubero_gazebo/src/actor_plugin_gazebo.cpp
+++ hubero_gazebo/src/actor_plugin_gazebo.cpp
@@ -4,18 +4,18 @@ GZ_REGISTER_MODEL_PLUGIN(gazebo::ActorPlugin)
 
 namespace gazebo {
 
-ActorPlugin::ActorPlugin()/*:
-	sim_animation_control_(std::make_shared<hubero::AnimationControlBase>()),
-	sim_localisation_(std::make_shared<hubero::LocalisationBase>()),
-	sim_model_control_(std::make_shared<hubero::ModelControlBase>()),
-	sim_world_geometry_(std::make_shared<hubero::WorldGeometryBase>()),
-	sim_time_(std::make_shared<hubero::TimeBase>())*/
-{ // : controller_enabled_(false) { 
-	// localisation_ptr_ = std::make_shared<hubero::interface::LocalisationGazebo>();
-}
+ActorPlugin::ActorPlugin():
+	controller_enabled_(false),
+	sim_animation_control_(std::make_shared<hubero::AnimationControlGazebo>()),
+	sim_localisation_(std::make_shared<hubero::LocalisationGazebo>()),
+	sim_model_control_(std::make_shared<hubero::ModelControlGazebo>()),
+	sim_world_geometry_(std::make_shared<hubero::WorldGeometryGazebo>())
+{}
 
 void ActorPlugin::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf) {
-	// plugin-related setup
+	/*
+	 * Gazebo plugin-related setup
+	 */
 	sdf_ptr_ = sdf;
 	model_ptr_ = model;
 	actor_ptr_ = boost::dynamic_pointer_cast<physics::Actor>(model);
@@ -28,59 +28,72 @@ void ActorPlugin::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf) {
 		)
 	);
 
-	// HuBeRo framework-related setup
-    std::cout << "LOADED POSE: " << actor_ptr_->WorldPose() << std::endl;
-	// actor_ptr_ = std::make_shared<actor::core::Actor>();
+	/*
+	 * HuBeRo framework interface initialization
+	 */
+	initializeHuberoInterface();
+	hubero_actor_.initializeSim(actor_ptr_->GetName(), sim_animation_control_, sim_model_control_, sim_localisation_);
 
-	// localisation_ptr_->initialize("world");
-	// actor_ptr_->initialize(actor->GetName());
-	// TODO: actor_ptr's interface should only allow interfaces to:
-	// - virtual receptor
-	// - virtual effector
-	// - interagent communication channels
+	/*
+	 * Enable specific trajectory of actor
+	 * It must be done in "Load", otherwise default animation (running) will be triggered and all actors in the 'world'
+	 * will be located in exact the same place
+	 */
+	actor_ptr_->SetCustomTrajectory(sim_animation_control_->getTrajectoryInfo());
 
-	// actor_ptr_->initGazeboInterface(actor, world);
-	// actor_ptr_->initRosInterface();
-	// actor_ptr_->initActor(sdf);
-	// std::cout << "MODDED POSE: " << this->actor->WorldPose() << std::endl;
-
+	/*
+	 * Update pose. Note that coordinate system of the human model is different to ROS REP 105
+	 * https://www.ros.org/reps/rep-0105.html
+	 */
+	std::cout << "LOADED POSE: " << actor_ptr_->WorldPose() << std::endl;
+	sim_localisation_->update(actor_ptr_->WorldPose());
+	actor_ptr_->SetWorldPose(sim_localisation_->getPoseTransformed());
 }
 
 void ActorPlugin::Reset() {
 
 }
 
-void ActorPlugin::OnUpdate(const gazebo::common::UpdateInfo &_info) {
+void ActorPlugin::initializeHuberoInterface() {
+	// animation control
+	sim_animation_control_->initialize(
+		actor_ptr_->SkeletonAnimations(),
+		// TODO: param
+		hubero::ANIMATION_STAND
+	);
+
+	// localisation
+	// TODO: param
+	sim_localisation_->initialize("world");
+
+	// model control
+	// TODO: param
+	sim_model_control_->initialize(actor_ptr_, "world");
+
+	// world geometry
+	// TODO: param
+	sim_world_geometry_->initialize("world", world_ptr_, actor_ptr_->GetName());
+}
+
+void ActorPlugin::OnUpdate(const common::UpdateInfo& info) {
 	/*
-	// NOTE: `_info.realTime.Double()` returns seconds from the launch
-	//
-	// Not waiting for the ROS to initialize produces further problems
-	// (mainly with GetCostmapStatus service). The longest task in the
-	// `actor_global_plan_node` is the costmap initialization which
-	// somehow blocks processing callbacks from services (probably ros::spin()
-	// is called after costmap's initialization and that creates this bad behaviour).
-	// To check what did not help, check these commits:
-	// https://github.com/rayvburn/gazebo_ros_people_sim/commit/efc52f04b2c10d764db78295e1a7f3c423a9c26e
-	// https://github.com/rayvburn/gazebo_ros_people_sim/commit/62c8719b25aac5aff53447ba483045364fcd783f
-	// After many tries brought back some delay as below:
-	if ( !controller_enabled_ ) {
-		if ( _info.realTime.Double() >= 8.0 ) {
+	 * This is very naive but the most effective way to prepare both ROS and Gazebo for typical operation.
+	 */
+	if (!controller_enabled_) {
+		// returns seconds since plugin load
+		if (info.realTime.Double() >= 8.0 ) {
 			std::cout << "\t[ActorPlugin] Actor controller starting the job!" << std::endl;
 			controller_enabled_ = true;
 		}
 		return;
 	}
 
-	// ???????????
-	if ( !controller_enabled_ ) {
-		SfmSetPrintData(false); // for some reason - REMOVING THIS completely FROM onUpdate makes Gazebo crash
-		return;
-	};
-
-	localisation_ptr_->update(this->actor->WorldPose());
-	actor_ptr_->executeTransitionFunction();
-	*/
-  	return;
+	/*
+	 * Handle simulation update
+	 */
+	sim_localisation_->update(actor_ptr_->WorldPose());
+	hubero::Time time(info.simTime.Double());
+	hubero_actor_.update(time);
 }
 
 } // namespace gazebo
