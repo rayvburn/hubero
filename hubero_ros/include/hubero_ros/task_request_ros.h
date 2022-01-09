364711251bdbbdec1bfa58e7f92605bbb34e98b4
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-01-09 22:47:50 +0100

    hubero_ros - added TaskRequestROS class, initial version of action callback handler (template)

---
+++ hubero_ros/include/hubero_ros/task_request_ros.h
@@ -0,0 +1,132 @@
+#pragma once
+
+#include <hubero_interfaces/task_request_base.h>
+#include <hubero_ros/node.h>
+
+#include <hubero_ros_msgs/FollowObjectAction.h>
+#include <hubero_ros_msgs/LieDownAction.h>
+#include <hubero_ros_msgs/LieDownObjectAction.h>
+#include <hubero_ros_msgs/MoveToGoalAction.h>
+#include <hubero_ros_msgs/MoveToObjectAction.h>
+
+#include <actionlib/server/simple_action_server.h>
+#include <actionlib_msgs/GoalStatus.h>
+
+#include <chrono>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <thread>
+
+namespace hubero {
+
+/**
+ * @brief Provides ROS interface to request tasks from actors and allows to monitor execution using feedback data
+ */
+class TaskRequestROS: public TaskRequestBase {
+public:
+	/// Alias declaration
+	template <typename T>
+	using ActionServer = actionlib::SimpleActionServer<T>;
+
+	/// Alias declaration
+	template <typename T>
+	using ActionServerPtr = std::shared_ptr<ActionServer<T>>;
+
+	/// Contains suffix attached to basic action name, e.g., MoveToGoal vs MoveToObject
+	const std::string OBJECT_ORIENTED_TASK_SUFFIX = "_name";
+
+	/// How often task feedback should be published
+	const std::chrono::milliseconds TASK_FEEDBACK_PERIOD = std::chrono::milliseconds(500);
+
+	/**
+	 * @brief Default constructor
+	 */
+	TaskRequestROS();
+
+	/**
+	 * @brief Initializes ROS interface to request tasks from simulated actor, named @ref actor_name
+	 */
+	void initialize(std::shared_ptr<Node> node_ptr, const std::string& actor_name);
+
+protected:
+	/**
+	 * @brief Shared node handle
+	 */
+	std::shared_ptr<Node> node_ptr_;
+
+	/**
+	 * @defgroup actions ROS actions
+	 * @{
+	 */
+	ActionServerPtr<hubero_ros_msgs::FollowObjectAction> as_follow_object_;
+	ActionServerPtr<hubero_ros_msgs::LieDownAction> as_lie_down_;
+	ActionServerPtr<hubero_ros_msgs::LieDownObjectAction> as_lie_down_object_;
+	ActionServerPtr<hubero_ros_msgs::MoveToGoalAction> as_move_to_goal_;
+	ActionServerPtr<hubero_ros_msgs::MoveToObjectAction> as_move_to_object_;
+	/// @}
+
+	/**
+	 * @defgroup actioncallbacks Callbacks for each action
+	 * @{
+	 */
+	void actionCbFollowObject(const hubero_ros_msgs::FollowObjectGoalConstPtr& goal);
+	void actionCbMoveToGoal(const hubero_ros_msgs::MoveToGoalGoalConstPtr& goal);
+	void actionCbMoveToObject(const hubero_ros_msgs::MoveToObjectGoalConstPtr& goal);
+	void actionCbLieDown(const hubero_ros_msgs::LieDownGoalConstPtr& goal);
+	void actionCbLieDownObject(const hubero_ros_msgs::LieDownObjectGoalConstPtr& goal);
+	/// @}
+
+	/**
+	 * @brief Handler of the action goal callback
+	 *
+	 * @tparam Tresult type of the action result structure
+	 * @tparam Tfeedback type of the action feedback structure
+	 * @tparam Taction type of the action server
+	 * @param request_processed_ok whether initial request status is ok
+	 * @param task_type type of the task
+	 * @param as_ptr action server pointer
+	 */
+	template <typename Tresult, typename Tfeedback, typename Taction>
+	void actionCbHandler(bool request_processed_ok, TaskType task_type, ActionServerPtr<Taction>& as_ptr) {
+		// evaluate request initial response
+		if (!request_processed_ok || getTaskFeedbackType(task_type) == TASK_FEEDBACK_REJECTED) {
+			Tresult result;
+			result.text = "Rejected after initial check";
+			result.status = actionlib_msgs::GoalStatus::REJECTED;
+			as_ptr->setAborted(result);
+			return;
+		}
+
+		// wait until accepted
+		while (getTaskFeedbackType(task_type) == TASK_FEEDBACK_PENDING) {
+			std::this_thread::sleep_for(TASK_FEEDBACK_PERIOD);
+		}
+
+		// activated
+		TaskFeedbackType feedback_type = TASK_FEEDBACK_UNDEFINED;
+		while ((feedback_type = getTaskFeedbackType(task_type)) == TASK_FEEDBACK_ACTIVE) {
+			Tfeedback feedback;
+			feedback.status = feedback_type;
+			as_ptr->publishFeedback(feedback);
+			std::this_thread::sleep_for(TASK_FEEDBACK_PERIOD);
+		}
+
+		auto final_feedback_type = getTaskFeedbackType(task_type);
+		if (final_feedback_type == TASK_FEEDBACK_SUCCEEDED) {
+			as_ptr->setSucceeded();
+			return;
+		} else if (final_feedback_type == TASK_FEEDBACK_ABORTED) {
+			as_ptr->setAborted();
+			return;
+		}
+
+		std::runtime_error(
+			"failed to safely terminate requested task, finished with "
+			+ std::to_string(final_feedback_type)
+			+ " feedback type"
+		);
+	}
+}; // class TaskRequestRos
+
+} // namespace hubero
--- hubero_ros/src/task_request_ros.cpp
+++ hubero_ros/src/task_request_ros.cpp
@@ -1,1 +1,113 @@
-// use map to create relation between enum number of task type and string representation (topic)
+#include <hubero_ros/task_request_ros.h>
+
+namespace hubero {
+
+TaskRequestROS::TaskRequestROS(): TaskRequestBase::TaskRequestBase() {}
+
+void TaskRequestROS::initialize(std::shared_ptr<Node> node_ptr, const std::string& actor_name) {
+    if (node_ptr == nullptr) {
+        HUBERO_LOG("[TaskRequestROS] Pointer to Node is null, action servers will not be started\r\n");
+        return;
+    }
+
+    node_ptr_ = node_ptr;
+
+    // convert task IDs to task names
+    auto name_task_stand = TaskRequestBase::getTaskName(TASK_STAND);
+	auto name_task_move_to_goal = TaskRequestBase::getTaskName(TASK_MOVE_TO_GOAL);
+	auto name_task_move_around = TaskRequestBase::getTaskName(TASK_MOVE_AROUND);
+	auto name_task_lie_down = TaskRequestBase::getTaskName(TASK_LIE_DOWN);
+	auto name_task_sit_down = TaskRequestBase::getTaskName(TASK_SIT_DOWN);
+	auto name_task_follow_object = TaskRequestBase::getTaskName(TASK_FOLLOW_OBJECT);
+	auto name_task_teleop = TaskRequestBase::getTaskName(TASK_TELEOP);
+	auto name_task_run = TaskRequestBase::getTaskName(TASK_RUN);
+	auto name_task_talk = TaskRequestBase::getTaskName(TASK_TALK);
+
+    // create action servers
+    as_follow_object_ = std::make_shared<ActionServer<hubero_ros_msgs::FollowObjectAction>>(
+        *node_ptr_->getNodeHandlePtr(),
+        actor_name + "/" + name_task_follow_object,
+        std::bind(&TaskRequestROS::actionCbFollowObject, this, std::placeholders::_1),
+        false
+    );
+    as_follow_object_->start();
+
+    as_lie_down_ = std::make_shared<ActionServer<hubero_ros_msgs::LieDownAction>>(
+        *node_ptr_->getNodeHandlePtr(),
+        actor_name + "/" + name_task_lie_down,
+        std::bind(&TaskRequestROS::actionCbLieDown, this, std::placeholders::_1),
+        false
+    );
+    as_lie_down_->start();
+
+    as_lie_down_object_ = std::make_shared<ActionServer<hubero_ros_msgs::LieDownObjectAction>>(
+        *node_ptr_->getNodeHandlePtr(),
+        actor_name + "/" + name_task_lie_down + TaskRequestROS::OBJECT_ORIENTED_TASK_SUFFIX,
+        std::bind(&TaskRequestROS::actionCbLieDownObject, this, std::placeholders::_1),
+        false
+    );
+    as_lie_down_object_->start();
+
+    as_move_to_goal_ = std::make_shared<ActionServer<hubero_ros_msgs::MoveToGoalAction>>(
+        *node_ptr_->getNodeHandlePtr(),
+        actor_name + "/" + name_task_move_to_goal,
+        std::bind(&TaskRequestROS::actionCbMoveToGoal, this, std::placeholders::_1),
+        false
+    );
+    as_move_to_goal_->start();
+
+    as_move_to_object_ = std::make_shared<ActionServer<hubero_ros_msgs::MoveToObjectAction>>(
+        *node_ptr_->getNodeHandlePtr(),
+        actor_name + "/" + name_task_move_to_goal + TaskRequestROS::OBJECT_ORIENTED_TASK_SUFFIX,
+        std::bind(&TaskRequestROS::actionCbMoveToObject, this, std::placeholders::_1),
+        false
+    );
+    as_move_to_object_->start();
+}
+
+void TaskRequestROS::actionCbFollowObject(const hubero_ros_msgs::FollowObjectGoalConstPtr& goal) {
+    bool request_processed_ok = request(TASK_FOLLOW_OBJECT, goal->object_name);
+    actionCbHandler<hubero_ros_msgs::FollowObjectResult, hubero_ros_msgs::FollowObjectFeedback>(
+        request_processed_ok,
+        TASK_FOLLOW_OBJECT,
+        as_follow_object_
+    );
+}
+
+void TaskRequestROS::actionCbMoveToGoal(const hubero_ros_msgs::MoveToGoalGoalConstPtr& goal) {
+    bool request_processed_ok = false; // request(TASK_MOVE_TO_GOAL, goal->...);
+    actionCbHandler<hubero_ros_msgs::MoveToGoalResult, hubero_ros_msgs::MoveToGoalFeedback>(
+        request_processed_ok,
+        TASK_MOVE_TO_GOAL,
+        as_move_to_goal_
+    );
+}
+
+void TaskRequestROS::actionCbMoveToObject(const hubero_ros_msgs::MoveToObjectGoalConstPtr& goal) {
+    bool request_processed_ok = false; // request(TASK_MOVE_TO_GOAL, goal->...);
+    actionCbHandler<hubero_ros_msgs::MoveToObjectResult, hubero_ros_msgs::MoveToObjectFeedback>(
+        request_processed_ok,
+        TASK_MOVE_TO_GOAL,
+        as_move_to_object_
+    );
+}
+
+void TaskRequestROS::actionCbLieDown(const hubero_ros_msgs::LieDownGoalConstPtr& goal) {
+    bool request_processed_ok = false; // request(TASK_LIE_DOWN, goal->...);
+    actionCbHandler<hubero_ros_msgs::LieDownResult, hubero_ros_msgs::LieDownFeedback>(
+        request_processed_ok,
+        TASK_LIE_DOWN,
+        as_lie_down_
+    );
+}
+
+void TaskRequestROS::actionCbLieDownObject(const hubero_ros_msgs::LieDownObjectGoalConstPtr& goal) {
+    bool request_processed_ok = false; // request(TASK_LIE_DOWN, goal->...);
+    actionCbHandler<hubero_ros_msgs::LieDownObjectResult, hubero_ros_msgs::LieDownObjectFeedback>(
+        request_processed_ok,
+        TASK_LIE_DOWN,
+        as_lie_down_object_
+    );
+}
+
+} // namespace hubero
