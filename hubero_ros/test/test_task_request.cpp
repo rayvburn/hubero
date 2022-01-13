6f241c4636a6d20e781933d0e3e53ffc78fcb401
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-01-13 21:23:19 +0100

    hubero_ros - added unit test for TaskRequestRos

---
+++ hubero_ros/test/test_task_request.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include <hubero_ros/task_request_ros.h>
+
+using namespace hubero;
+
+TEST(HuberoTaskRequestRos, taskRequest) {
+	auto node = std::make_shared<Node>();
+    auto task = std::make_shared<TaskBase>(TASK_STAND);
+    TaskRequestRos task_request;
+    task_request.addTask(TASK_STAND, task);
+    task_request.initialize(node, "test");
+
+    ASSERT_FALSE(task->isRequested());
+    task_request.request(task_request.getTaskName(TASK_STAND));
+    ASSERT_TRUE(task->isRequested());
+}
+
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
