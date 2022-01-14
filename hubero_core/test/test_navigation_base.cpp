833851718e7833ad44361a434359210e71291afa
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-01-15 00:15:13 +0100

    hubero_core - NavigationBase class unit-tested

---
+++ hubero_core/test/test_navigation_base.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include <hubero_interfaces/navigation_base.h>
+
+#include <iostream>
+
+using namespace hubero;
+
+TEST(HuberoNav, initialState) {
+    NavigationBase nav;
+    ASSERT_FALSE(nav.isInitialized());
+    nav.initialize("test");
+    ASSERT_TRUE(nav.isInitialized());
+    ASSERT_EQ(nav.getFeedback(), TaskFeedbackType::TASK_FEEDBACK_UNDEFINED);
+    ASSERT_FALSE(nav.isPoseAchievable(Pose3(), Pose3()));
+}
+
+int main(int argc, char** argv) {
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
