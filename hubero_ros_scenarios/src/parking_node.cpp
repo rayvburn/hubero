448b3e4753944f12095da7738f8884d4a03fa5df
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-01-14 20:32:42 +0100

    hubero_ros_scenarios - added initial version of the package with 2 exemplary scenarios

---
+++ hubero_ros_scenarios/src/parking_node.cpp
@@ -0,0 +1,31 @@
+#include <ros/ros.h>
+#include <string>
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+#include <hubero_ros/task_request_ros_api.h>
+
+int main(int argc, char** argv) {
+	// node initialization
+	ros::init(argc, argv, "parking_scenario_node");
+	ros::NodeHandle nh;
+
+	// check if an extra (necessary) argument(s) provided
+	long int launch_delay = 30000;//0;
+	if ( argc >= 2 ) {
+		launch_delay = 1000 * std::stoi(argv[1]);
+	}
+
+	// create interfaces to request tasks from actors
+	hubero::TaskRequestRosApi actor1("actor1");
+	hubero::TaskRequestRosApi actor2("actor2");
+	hubero::TaskRequestRosApi actor3("actor3");
+	hubero::TaskRequestRosApi actor4("actor4");
+
+	// wait
+	std::this_thread::sleep_for(std::chrono::milliseconds(launch_delay));
+
+	ROS_INFO("Scenario operation finished!");
+	return 0;
+}
