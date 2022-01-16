420bdfe8705d65609255546e0ba7b465774e0590
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-01-16 02:43:51 +0100

    hubero_gazebo - added doxy to LocalisationGazebo class

--- hubero_gazebo/include/hubero_gazebo/localisation_gazebo.h
+++ hubero_gazebo/include/hubero_gazebo/localisation_gazebo.h
@@ -4,6 +4,12 @@
 
 namespace hubero {
 
+/**
+ * @brief Gazebo-specific interface to provide localisation data from simulator to HuBeRo
+ *
+ * @details There are multiple problems with Actor Plugin. One of them is that relative pose of sensor links (that were
+ * attached to human body) cannot be read from the plugin level. See a workaround in `bringup` package
+ */
 class LocalisationGazebo: public LocalisationBase {
 public:
     LocalisationGazebo();
