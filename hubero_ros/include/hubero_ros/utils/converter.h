56a855a7015b49ab0da7175facdc70df280728ce
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-01-21 14:03:33 +0100

    hubero_ros - added geom_msgs::Tf to Pose conversion function, headers cleanup

--- hubero_ros/include/hubero_ros/utils/converter.h
+++ hubero_ros/include/hubero_ros/utils/converter.h
@@ -2,8 +2,8 @@
 
 #include <hubero_common/typedefs.h>
 
-#include <geometry_msgs/PoseStamped.h>
-#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/Transform.h>
 #include <geometry_msgs/Twist.h>
 
 namespace hubero {
@@ -12,5 +12,6 @@ geometry_msgs::Pose ignPoseToMsgPose(const Pose3& pose);
 geometry_msgs::Transform ignPoseToMsgTf(const Pose3& pose);
 geometry_msgs::Twist ignVectorsToMsgTwist(const Vector3& vel_lin, const Vector3& vel_ang);
 Vector3 msgTwistToIgnVector(const geometry_msgs::Twist& twist);
+Pose3 msgTfToPose(const geometry_msgs::Transform& tf);
 
 } // namespace hubero
