98fbe54df59006c4a911a51fe11f52fe19523289
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-01-21 13:56:41 +0100

    hubero_ros - NavigationRos: TF buffer complemented with TF listener

--- hubero_ros/include/hubero_ros/navigation_ros.h
+++ hubero_ros/include/hubero_ros/navigation_ros.h
@@ -6,6 +6,7 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <tf2_ros/buffer.h>
+#include <tf2_ros/transform_listener.h>
 #include <nav_msgs/GetPlan.h>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
@@ -121,6 +122,7 @@ protected:
 	 * @defgroup tf Transform frames
 	 */
 	tf2_ros::Buffer tf_buffer_;
+	tf2_ros::TransformListener tf_listener_;
 	tf2_ros::TransformBroadcaster tf_broadcaster_;
 	std::string frame_base_;
 	std::string frame_global_ref_;
