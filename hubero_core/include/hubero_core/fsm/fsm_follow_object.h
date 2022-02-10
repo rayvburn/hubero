3c7ce886249b7bc2b144c1a66ca2fb750ec82ce4
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-02-11 00:44:04 +0100

    hubero_core - extended follow object task FSM (extra state to trigger 'setup' action)

--- hubero_core/include/hubero_core/fsm/fsm_follow_object.h
+++ hubero_core/include/hubero_core/fsm/fsm_follow_object.h
@@ -12,10 +12,11 @@ class FsmFollowObject: public fsmlite::fsm<FsmFollowObject>, public FsmEssential
 public:
 	enum State {
 		MOVING_TO_GOAL = 0,
-		WAITING_FOR_MOVEMENT
+		WAITING_FOR_MOVEMENT,
+		FINISHED
 	};
 
-	FsmFollowObject(State state_init = State::MOVING_TO_GOAL): fsm(state_init), FsmEssentials("FsmFollowObject") {}
+	FsmFollowObject(State state_init = State::FINISHED): fsm(state_init), FsmEssentials("FsmFollowObject") {}
 
 protected:
 	/**
@@ -23,12 +24,25 @@ protected:
 	 * @{
 	 */
 	bool guardMoveToObject2Wait(const EventFsmFollowObject& event) const {
-		return event.isObjectNearby();
+		return event.isObjectNearby() && event.isActive();
 	}
 
 	bool guardWait2MoveToObject(const EventFsmFollowObject& event) const {
-		return !event.isObjectNearby();
+		return !event.isObjectNearby() && event.isActive();
 	}
+
+	bool guardMoveToObject2Finished(const EventFsmFollowObject& event) const {
+		return !event.isActive();
+	}
+
+	bool guardWait2Finished(const EventFsmFollowObject& event) const {
+		return !event.isActive();
+	}
+
+	bool guardFinished2MoveToObject(const EventFsmFollowObject& event) const {
+		return event.isActive();
+	}
+
 	/** @} */ // end of guards group
 
 	/**
@@ -44,6 +58,22 @@ protected:
 		logTransition("WAITING FOR MOVEMENT", "MOVING TO OBJECT", event);
 		transitionHandler(State::WAITING_FOR_MOVEMENT, State::MOVING_TO_GOAL);
 	}
+
+	void transHandlerMoveToObject2Finished(const EventFsmFollowObject& event) {
+		logTransition("MOVING TO OBJECT", "FINISHED", event);
+		transitionHandler(State::MOVING_TO_GOAL, State::FINISHED);
+	}
+
+	void transHandlerWait2Finished(const EventFsmFollowObject& event) {
+		logTransition("WAITING FOR MOVEMENT", "FINISHED", event);
+		transitionHandler(State::WAITING_FOR_MOVEMENT, State::FINISHED);
+	}
+
+	void transHandlerFinished2MoveToObject(const EventFsmFollowObject& event) {
+		logTransition("FINISHED", "MOVING TO OBJECT", event);
+		transitionHandler(State::FINISHED, State::MOVING_TO_GOAL);
+	}
+
 	/** @} */ // end of transition handlers group
 
 	/**
@@ -52,7 +82,10 @@ protected:
 	 */
 	using transition_table = table<
 		mem_fn_row<State::MOVING_TO_GOAL, EventFsmFollowObject, State::WAITING_FOR_MOVEMENT, &FsmFollowObject::transHandlerMoveToObject2Wait, &FsmFollowObject::guardMoveToObject2Wait>,
-		mem_fn_row<State::WAITING_FOR_MOVEMENT, EventFsmFollowObject, State::MOVING_TO_GOAL, &FsmFollowObject::transHandlerWait2MoveToObject, &FsmFollowObject::guardWait2MoveToObject>
+		mem_fn_row<State::WAITING_FOR_MOVEMENT, EventFsmFollowObject, State::MOVING_TO_GOAL, &FsmFollowObject::transHandlerWait2MoveToObject, &FsmFollowObject::guardWait2MoveToObject>,
+		mem_fn_row<State::MOVING_TO_GOAL, EventFsmFollowObject, State::FINISHED, &FsmFollowObject::transHandlerMoveToObject2Finished, &FsmFollowObject::guardMoveToObject2Finished>,
+		mem_fn_row<State::WAITING_FOR_MOVEMENT, EventFsmFollowObject, State::FINISHED, &FsmFollowObject::transHandlerWait2Finished, &FsmFollowObject::guardWait2Finished>,
+		mem_fn_row<State::FINISHED, EventFsmFollowObject, State::MOVING_TO_GOAL, &FsmFollowObject::transHandlerFinished2MoveToObject, &FsmFollowObject::guardFinished2MoveToObject>
 	>;
 
 	friend class fsmlite::fsm<FsmFollowObject>;
