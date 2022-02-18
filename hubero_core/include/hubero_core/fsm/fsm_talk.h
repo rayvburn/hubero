82e818a47a95cbe7f8a04a2a800e1ec4743b9b5e
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-02-18 18:35:46 +0100

    hubero_core - talk task class filled up, FSM updated (does not allow moving towards goal after start of talk)

--- hubero_core/include/hubero_core/fsm/fsm_talk.h
+++ hubero_core/include/hubero_core/fsm/fsm_talk.h
@@ -16,39 +16,59 @@ class FsmTalk: public fsmlite::fsm<FsmTalk>, public FsmEssentials {
 public:
 	enum State {
 		MOVING_TO_GOAL = 0,
-		TALKING
+		TALKING,
+		FINISHED
 	};
 
-	FsmTalk(State state_init = State::MOVING_TO_GOAL): fsm(state_init), FsmEssentials("FsmTalk") {}
+	FsmTalk(State state_init = State::FINISHED): fsm(state_init), FsmEssentials("FsmTalk") {}
 
 protected:
 	/**
 	 * @defgroup guards FSM transition guards
 	 * @{
 	 */
+	bool guardFinished2MovingToGoal(const EventFsmTalk& event) const {
+		return event.isActive();
+	}
+
+	bool guardMovingToGoal2Finished(const EventFsmTalk& event) const {
+		return !event.isActive();
+	}
+
 	bool guardMovingToGoal2Talking(const EventFsmTalk& event) const {
 		return event.isNavigationSucceeded();
 	}
 
-	bool guardTalking2MovingToGoal(const EventFsmTalk& event) const {
-		// FIXME: this is not the best solution if task will be dynamic-object-oriented
-		return event.isEnded();
+	bool guardTalking2Finished(const EventFsmTalk& event) const {
+		return !event.isActive();
 	}
+
 	/** @} */ // end of guards group
 
 	/**
 	 * @defgroup Transition handlers
 	 * @{
 	 */
+	void transHandlerFinished2MovingToGoal(const EventFsmTalk& event) {
+		logTransition("FINISHED", "MOVING TO POSITION", event);
+		transitionHandler(State::FINISHED, State::MOVING_TO_GOAL);
+	}
+
+	void transHandlerMovingToGoal2Finished(const EventFsmTalk& event) {
+		logTransition("MOVING TO POSITION", "FINISHED", event);
+		transitionHandler(State::MOVING_TO_GOAL, State::FINISHED);
+	}
+
 	void transHandlerMovingToGoal2Talking(const EventFsmTalk& event) {
 		logTransition("MOVING TO POSITION", "TALKING", event);
 		transitionHandler(State::MOVING_TO_GOAL, State::TALKING);
 	}
 
-	void transHandlerTalking2MovingToGoal(const EventFsmTalk& event) {
-		logTransition("TALKING", "MOVING TO POSITION", event);
-		transitionHandler(State::TALKING, State::MOVING_TO_GOAL);
+	void transHandlerTalking2Finished(const EventFsmTalk& event) {
+		logTransition("TALKING", "FINISHED", event);
+		transitionHandler(State::TALKING, State::FINISHED);
 	}
+
 	/** @} */ // end of transition handlers group
 
 	/**
@@ -56,8 +76,10 @@ protected:
 	 * @details Guard returning true allows transition between states to happen.
 	 */
 	using transition_table = table<
+		mem_fn_row<State::FINISHED, EventFsmTalk, State::MOVING_TO_GOAL, &FsmTalk::transHandlerFinished2MovingToGoal, &FsmTalk::guardFinished2MovingToGoal>,
+		mem_fn_row<State::MOVING_TO_GOAL, EventFsmTalk, State::FINISHED, &FsmTalk::transHandlerMovingToGoal2Finished, &FsmTalk::guardMovingToGoal2Finished>,
 		mem_fn_row<State::MOVING_TO_GOAL, EventFsmTalk, State::TALKING, &FsmTalk::transHandlerMovingToGoal2Talking, &FsmTalk::guardMovingToGoal2Talking>,
-		mem_fn_row<State::TALKING, EventFsmTalk, State::MOVING_TO_GOAL, &FsmTalk::transHandlerTalking2MovingToGoal, &FsmTalk::guardTalking2MovingToGoal>
+		mem_fn_row<State::TALKING, EventFsmTalk, State::FINISHED, &FsmTalk::transHandlerTalking2Finished, &FsmTalk::guardTalking2Finished>
 	>;
 
 	friend class fsmlite::fsm<FsmTalk>;
