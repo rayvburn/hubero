ad2a9607f07c9df755ccf488b5367fa5a137b5db
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-02-18 18:35:46 +0100

    hubero_core - talk task class filled up, FSM updated (does not allow moving towards goal after start of talk)

--- hubero_core/include/hubero_core/tasks/task_talk.h
+++ hubero_core/include/hubero_core/tasks/task_talk.h
@@ -14,9 +14,36 @@ public:
 	{
 		task_args_num_ = countArgumentsNum(&TaskTalk::request);
 		state_bb_map_ = {
+			{FsmTalk::State::FINISHED, BasicBehaviourType::BB_STAND},
 			{FsmTalk::State::MOVING_TO_GOAL, BasicBehaviourType::BB_MOVE_TO_GOAL},
 			{FsmTalk::State::TALKING, BasicBehaviourType::BB_TALK}
 		};
+
+		fsm_.addTransitionHandler(
+			TaskTalk::State::FINISHED,
+			TaskTalk::State::MOVING_TO_GOAL,
+			std::bind(&TaskTalk::thSetupNavigation, this)
+		);
+		fsm_.addTransitionHandler(
+			TaskTalk::State::FINISHED,
+			TaskTalk::State::MOVING_TO_GOAL,
+			std::bind(&TaskTalk::thSetupAnimation, this, ANIMATION_WALK)
+		);
+		fsm_.addTransitionHandler(
+			TaskTalk::State::MOVING_TO_GOAL,
+			TaskTalk::State::TALKING,
+			std::bind(&TaskTalk::thSetupAnimation, this, ANIMATION_TALK)
+		);
+		fsm_.addTransitionHandler(
+			TaskTalk::State::MOVING_TO_GOAL,
+			TaskTalk::State::FINISHED,
+			std::bind(&TaskTalk::thSetupAnimation, this, ANIMATION_STAND)
+		);
+		fsm_.addTransitionHandler(
+			TaskTalk::State::TALKING,
+			TaskTalk::State::FINISHED,
+			std::bind(&TaskTalk::thSetupAnimation, this, ANIMATION_STAND)
+		);
 	}
 
 	virtual bool request(const Pose3& goal) override {
@@ -39,6 +66,11 @@ public:
 	}
 
 protected:
+	virtual void updateMemory() override {
+		memory_ptr_->setGoal(getGoal());
+		TaskEssentials::updateMemory();
+	}
+
 	Pose3 goal_;
 
 	double goal_reached_distance_;
