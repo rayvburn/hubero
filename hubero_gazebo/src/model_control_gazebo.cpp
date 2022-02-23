554b388f395fea8f69c0d18ee79f5f610299222d
rayvburn <j.karwowski@stud.elka.pw.edu.pl>
2022-02-23 18:14:47 +0100

    hubero_gazebo - ModelControl updated with SetForce and SetTorque (for specific link) instead of SetAccelerations for a whole model [#44]

--- hubero_gazebo/src/model_control_gazebo.cpp
+++ hubero_gazebo/src/model_control_gazebo.cpp
@@ -1,5 +1,6 @@
 #include <hubero_gazebo/model_control_gazebo.h>
 #include <functional>
+#include <gazebo/physics/Link.hh>
 
 namespace hubero {
 
@@ -8,7 +9,9 @@ ModelControlGazebo::ModelControlGazebo(): ModelControlBase::ModelControlBase() {
 void ModelControlGazebo::initialize(gazebo::physics::ActorPtr& actor_ptr, const std::string& frame_id) {
     // for simplicity
     typedef gazebo::physics::Actor GazeboActor;
+    typedef gazebo::physics::Link GazeboLink;
 
+    // NOTE: explicit ignition::math objects given here
     // NOTE: 2 superfluous arguments are hard-coded here
     auto fun_world_pose = std::bind(
         static_cast<void(GazeboActor::*)(const Pose3&, const bool, const bool)>(&GazeboActor::SetWorldPose),
@@ -25,14 +28,27 @@ void ModelControlGazebo::initialize(gazebo::physics::ActorPtr& actor_ptr, const 
         actor_ptr,
         std::placeholders::_1
     );
+
+    /*
+     * Previously GazeboActor::SetLinearAccel and GazeboActor::SetAngularAccel were called but newer Gazebo warns:
+     *   [Wrn] [Model.cc:732] Model::SetAngularAccel() is deprecated and has no effect.
+     *   Use Link::SetTorque() on the link directly instead.
+     */
+    // Virtual human body skeleton - link name is standardized
+    auto hips_link = actor_ptr->GetLink("Hips");
+    /*
+     * Linear acceleration (force) and angular acceleration (torque) will likely not affect character
+     * based on Gazebo's ActorPlugin since the plugin does not handle dynamics in any kind
+     */
     auto fun_linear_accel = std::bind(
-        static_cast<void(GazeboActor::*)(const Vector3&)>(&GazeboActor::SetLinearAccel),
-        actor_ptr,
+        static_cast<void(GazeboLink::*)(const Vector3&)>(&GazeboLink::SetForce),
+        hips_link,
         std::placeholders::_1
     );
+    // angular acceleration - torque is updated here
     auto fun_angular_accel = std::bind(
-        static_cast<void(GazeboActor::*)(const Vector3&)>(&GazeboActor::SetAngularAccel),
-        actor_ptr,
+        static_cast<void(GazeboLink::*)(const Vector3&)>(&GazeboLink::SetTorque),
+        hips_link,
         std::placeholders::_1
     );
 
